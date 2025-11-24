9f30b0bab00efb1bf791a933c7fb95e198199325
okworemmanuelc <okworchimezie@gmail.com>
2025-11-24 08:22:14 +0100

    started light-box project

--- real-time-character-counter/script.js
+++ real-time-character-counter/script.js
@@ -1,4 +1,4 @@
-const charCount = document.getElementById('char-count');
+    const charCount = document.getElementById('char-count');
 const textInput = document.getElementById('text-input');
 const count = textInput.value.length;
 
@@ -12,9 +12,9 @@ const maxChars = () => {
     const count = textInput.value.length;
     if (textInput.value.length >= 50) {
         textInput.value = textInput.value.slice(0, 50);
-        charCount.textContent = `50/50`;
-        charCount.classList.add('error');
+        charCount.textContent = `Character Count: 50/50`;
+        charCount.style.color = 'red';
     } else {
-        charCount.textContent = `${count}/50`;
-        charCount.classList.remove('error');
+        charCount.textContent = `Character Count: ${count}/50`;
+        charCount.style.color = '#006ba0';
     }}
