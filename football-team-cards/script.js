3285e1c1c7c79272d5a6525ca087f0337ea384ce
okworemmanuelc <okworchimezie@gmail.com>
2025-11-25 17:41:18 +0100

    Started Football Team Cards project

---
+++ football-team-cards/script.js
@@ -0,0 +1,18 @@
+const footballTeam = {
+    team: "Napoli",
+    year: 2025,
+    players: [
+        { name: "Victor Osimhen", position: "forward", isCaptain: true },
+        { name: "Lorenzo Insigne", position: "forward", isCaptain: false },
+        { name: "Kalidou Koulibaly", position: "defender", isCaptain: false },
+        { name: "Fabian Ruiz", position: "midfielder", isCaptain: false },
+        { name: "David Ospina", position: "goalkeeper", isCaptain: false },
+        { name: "Piotr Zieliński", position: "midfielder", isCaptain: false },
+        { name: "Giovanni Di Lorenzo", position: "defender", isCaptain: false },
+        { name: "Hirving Lozano", position: "forward", isCaptain: false },
+        { name: "Andre-Frank Zambo Anguissa", position: "midfielder", isCaptain: false },
+        { name: "Matteo Politano", position: "forward", isCaptain: false },
+        { name: "Kostas Manolas", position: "defender", isCaptain: false }
+    ], 
+    headCoach: "Luciano Spalletti"
+}
